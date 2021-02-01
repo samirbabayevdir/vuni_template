b3f6306694be73ab4bf8a3d4c7a9bbc16511131b
Samir <samirbabayevdir@gmail.com>
2021-02-01 23:52:32 +0400

    header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,24 @@
-$(document).ready(function() {
-  $('.header__burger,.header__bg').click(function(event){
+// Burger
+$(document).ready(function () {
+  $('.header__burger,.header__bg').click(function (event) {
     $('.header__burger,.header__menu,.header__bg').toggleClass('active');
     $('body').toggleClass('lock');
   });
+
+  $(".mobile__acc").dcAccordion({
+    speed: 300
+  });
+
+  $(window).scroll(function () {
+    var $heightScrolled = $(window).scrollTop();
+    var $defaultHeight = 0;
+
+    if ($heightScrolled > $defaultHeight) {
+      $('.header').addClass("transition");
+    }
+    else {
+      $('.header').removeClass("transition");
+    }
+  });
 });
+
