ca73c242e32ead59bc7baefb90404c8a73368524
Samir <samirbabayevdir@gmail.com>
2021-02-04 22:56:13 +0400

    products

---
+++ products.php
@@ -0,0 +1,75 @@
+<?php
+include('header.php');
+?>
+<section class="banner__wrapper categories__page">
+  <div class="banner page banner__home">
+    <div class="banner__bg" style="background-image: url(/assets/images/ayaqqabi.jpg);"></div>
+    <div class="container">
+      <div class="row">
+        <div class="banner__inner">
+          <div class="banner__title">VEKTOR UNİFORMA</div>
+          <h1 class="banner__title-h1">Yay Ayaqqabıları</h1>
+        </div>
+      </div>
+    </div>
+    <div class="banner__skewed-bar"></div>
+  </div>
+</section>
+<section class="categories">
+  <div class="container">
+    <div class="row categories__header">
+      <div class="col-lg-6">
+        <h2>Məhsullar</h2>
+      </div>
+    </div>
+    <div class="categories__list">
+      <div class="categories__module">VEKTOR UNİFORMA</div>
+      <div class="row justify-content-center">
+        <div class="col-lg-4">
+          <div class="categories__box">
+            <a href="#" style="background-image: url(/assets/images/ydsone.jpg);">
+              <div class="categories__box-bg"></div>
+              <div class="btn__vk">YDS / ELPP 1090 S3 </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="categories__box">
+            <a href="#" style="background-image: url(/assets/images/ydstwo.jpg);">
+              <div class="categories__box-bg"></div>
+              <div class="btn__vk">YDS / ELPP 1065</div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="categories__box">
+            <a href="#" style="background-image: url(/assets/images/ydsthree.jpg);">
+              <div class="categories__box-bg"></div>
+              <div class="btn__vk">YDS / ELPP 1065</div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="categories__box">
+            <a href="#" style="background-image: url(/assets/images/talans.png);">
+              <div class="categories__box-bg"></div>
+              <div class="btn__vk">TALAN / ONE78</div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="categories__box">
+            <a href="#" style="background-image: url(/assets/images/babak.jpg);">
+              <div class="categories__box-bg"></div>
+              <div class="btn__vk">BABAK / THE892</div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="categories__skewed-bar"></div>
+</section>
+<?php
+include('footer.php');
+?>
